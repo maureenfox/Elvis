35c63781cd016bf24cc540b04d61deeb360fa263
Maureen Fox <maureenfox2022@gmail.com>
2025-01-19 15:32:44 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Maureen Fox on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
